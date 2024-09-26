e2984e7f3c5464da36caf9f4cf102b8e7931feaf
Simão Neto <simaomenezesneto@gmail.com>
2024-09-26 21:29:51 +0100

    chore: Create bean to modelMapper and create class to setting apiResponse

--- src/main/java/com/menezes/neto/dreamshops/config/ShopConfig.java
+++ src/main/java/com/menezes/neto/dreamshops/config/ShopConfig.java
@@ -1,5 +1,6 @@
 package com.menezes.neto.dreamshops.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,6 @@ public class ShopConfig {
 
     @Bean
     public ModelMapper modelMapper(){
-        return new MoldeMapper();
+        return new ModelMapper();
     }
 }
