668a862b4e9f6c12344164253876de48007f7664
Simão Neto <simaomenezesneto@gmail.com>
2024-10-16 21:15:25 +0100

    feat: Create pojo User.java

---
+++ src/main/java/com/menezes/neto/dreamshops/model/User.java
@@ -0,0 +1,26 @@
+package com.menezes.neto.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @NaturalId
+    private String email;
+    private String password;
+}
