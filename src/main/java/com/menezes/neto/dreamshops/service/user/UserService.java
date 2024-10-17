c6586b2cafd2ae6a798a3783e4292dfc91f79562
Simão Neto <simaomenezesneto@gmail.com>
2024-10-17 22:03:30 +0100

    feat: Create Repository and Service to User

---
+++ src/main/java/com/menezes/neto/dreamshops/service/user/UserService.java
@@ -0,0 +1,27 @@
+package com.menezes.neto.dreamshops.service.user;
+
+import com.menezes.neto.dreamshops.model.User;
+import com.menezes.neto.dreamshops.request.AddUserRequest;
+import com.menezes.neto.dreamshops.request.UpdateUserRequest;
+
+public class UserService implements IUserService{
+    @Override
+    public User getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User add(AddUserRequest request) {
+        return null;
+    }
+
+    @Override
+    public User update(UpdateUserRequest request, Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
