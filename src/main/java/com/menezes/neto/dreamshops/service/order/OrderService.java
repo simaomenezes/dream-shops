cecbd015055e69a1cb417f1dbe3f340fa2be86fb
Simão Neto <simaomenezesneto@gmail.com>
2024-10-26 14:44:29 +0100

    feat: Finished services to flow orders

--- src/main/java/com/menezes/neto/dreamshops/service/order/OrderService.java
+++ src/main/java/com/menezes/neto/dreamshops/service/order/OrderService.java
@@ -1,6 +1,9 @@
 package com.menezes.neto.dreamshops.service.order;
 
+import aj.org.objectweb.asm.commons.Remapper;
+import com.menezes.neto.dreamshops.dto.OrderDTO;
 import com.menezes.neto.dreamshops.enums.OrdersStatus;
+import com.menezes.neto.dreamshops.exceptions.ResourceNotFoundException;
 import com.menezes.neto.dreamshops.model.Cart;
 import com.menezes.neto.dreamshops.model.Order;
 import com.menezes.neto.dreamshops.model.OrderItem;
@@ -9,6 +12,7 @@ import com.menezes.neto.dreamshops.repository.OrderRepository;
 import com.menezes.neto.dreamshops.repository.ProductRepository;
 import com.menezes.neto.dreamshops.service.cart.CartService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +27,7 @@ public class OrderService implements IOrderService{
     private final OrderRepository repository;
     private final CartService cartService;
     private final ProductRepository productRepository;
+    private ModelMapper modelMapper;
 
     @Transactional
     @Override
@@ -35,6 +40,23 @@ public class OrderService implements IOrderService{
         Order savedOrder = repository.save(order);
         cartService.clearCart(cartFound.getId());
         return savedOrder;
+    }
+
+    @Override
+    public OrderDTO getOrderById(Long orderId) {
+        return repository.findById(orderId)
+                .map(this::convertToDTO)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
+    }
+
+    private OrderDTO convertToDTO(Order order) {
+        return modelMapper.map(order, OrderDTO.class);
+    }
+
+    @Override
+    public List<OrderDTO> getUserOrdersById(Long userId) {
+        List<Order> orders = repository.findByUserId(userId);
+        return orders.stream().map(this::convertToDTO).toList();
     }
 
     private BigDecimal calculateTotalAmount(List<OrderItem> orderItems) {
