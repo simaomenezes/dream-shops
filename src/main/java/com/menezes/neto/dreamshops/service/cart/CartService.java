972fa1dba702a93fee1a1bb12f5790c099edfb6f
Simão Neto <simaomenezesneto@gmail.com>
2024-10-30 20:42:06 +0000

    feat: Finished flow to cart

--- src/main/java/com/menezes/neto/dreamshops/service/cart/CartService.java
+++ src/main/java/com/menezes/neto/dreamshops/service/cart/CartService.java
@@ -1,9 +1,12 @@
 package com.menezes.neto.dreamshops.service.cart;
 
+import com.menezes.neto.dreamshops.exceptions.ResourceNotFoundException;
 import com.menezes.neto.dreamshops.model.Cart;
+import com.menezes.neto.dreamshops.repository.CartItemRepository;
 import com.menezes.neto.dreamshops.repository.CartRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.concurrent.atomic.AtomicLong;
@@ -12,20 +15,30 @@ import java.util.concurrent.atomic.AtomicLong;
 @RequiredArgsConstructor
 public class CartService implements ICartService{
     private final CartRepository repository;
-
     private final AtomicLong cartIdGenerator = new AtomicLong(0);
+    private final CartItemRepository cartItemRepository;
+
     @Override
     public Cart getById(Long id) {
-        return repository.findById(id).get();
+        Cart cart = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cart not Found"));
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        return repository.save(cart);
     }
 
+    @Transactional
     @Override
     public void clearCart(Long id) {
+        Cart cart = getById(id);
+        cartItemRepository.deleteAllByCartId(id);
+        cart.getItems().clear();
+        repository.deleteById(id);
     }
 
     @Override
     public BigDecimal getTotalPrice(Long id) {
-        return null;
+        Cart cart = getById(id);
+        return cart.getTotalAmount();
     }
 
 
