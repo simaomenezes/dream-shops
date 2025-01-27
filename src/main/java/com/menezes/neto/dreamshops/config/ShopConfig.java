796af350314fa8cc32d978ea01292baaa9f5a74c
Simão Neto <simaomenezesneto@gmail.com>
2025-01-27 15:08:10 +0000

    feat: Make flow to the manager access with spring secutiry

--- src/main/java/com/menezes/neto/dreamshops/config/ShopConfig.java
+++ src/main/java/com/menezes/neto/dreamshops/config/ShopConfig.java
@@ -1,14 +1,36 @@
 package com.menezes.neto.dreamshops.config;
 
+import com.menezes.neto.dreamshops.security.jwt.JwtAuthEntryPoint;
+import com.menezes.neto.dreamshops.security.user.ShopUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class ShopConfig {
+
+    private final ShopUserDetailsService userDetailsService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+    private static final List<String> SECURED_URLS = List.of("/api/v1/carts/**", "/api/v1/carItems/**");
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/menezes/neto/dreamshops/model/User.java
+++ src/main/java/com/menezes/neto/dreamshops/model/User.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.NaturalId;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 @Getter
@@ -24,8 +26,23 @@ public class User {
     @NaturalId
     private String email;
     private String password;
+
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Cart cart;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Order> orders;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {
+            CascadeType.DETACH,
+            CascadeType.MERGE,
+            CascadeType.PERSIST,
+            CascadeType.REFRESH
+    })
+    @JoinTable(name = "user_roles", joinColumns =
+        @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns =
+                @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Collection<Role> roles = new HashSet<>();
 }
