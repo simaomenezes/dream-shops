280984b2b1fe9af7763a2045c0f795acfbc01d95
Simão Neto <simaomenezesneto@gmail.com>
2025-02-05 09:46:46 +0000

    "fix: Text about constant and add annotation to the class"

--- src/main/java/com/menezes/neto/dreamshops/security/jwt/JwtUtils.java
+++ src/main/java/com/menezes/neto/dreamshops/security/jwt/JwtUtils.java
@@ -17,10 +17,10 @@ import java.util.List;
 
 @Component
 public class JwtUtils {
-    @Value("${auth.toke.jwtSecret}")
+    @Value("${api.auth.token.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${auth.token.expirationInMils}")
+    @Value("${api.auth.token.expirationInMils}")
     private int expirationTime;
 
     public String generateTokenForUser(Authentication authentication){
